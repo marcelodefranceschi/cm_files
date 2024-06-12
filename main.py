a513165998f628e8ff17a1024d1fc6cca37c5ac0
marcelodefranceschi <151757219+marcelodefranceschi@users.noreply.github.com>
2024-06-12 13:59:40 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import os
+
+
+st.title("Cost Management June 2024")
+st.write("#### Page to download templates and upload back the Project's forecast")
+
+Tunnels_file = r"C:\Users\marcelo.defranceschi\OneDrive - Lower Thames Crossing\Attachments\T&A - Month end\24.06 JUN24\New_Pillar_Tunnels.xlsx"
+
+
+with open(Tunnels_file, "rb") as file:
+    btn = st.download_button(
+            label="Download Tunnels file",
+            data=file,
+            file_name="Tunnels.xlsx",
+                      )
+
+with open(Tunnels_file, "rb") as file:
+    btn = st.download_button(
+            label="Download Roads North file",
+            data=file,
+            file_name="Tunnels.xlsx",
+                      )
+
+with open(Tunnels_file, "rb") as file:
+    btn = st.download_button(
+            label="Download Kent Roads file",
+            data=file,
+            file_name="Tunnels.xlsx",
+                      )
+
+
+
+
+# Define the target directory path
+target_directory = r"C:\Users\marcelo.defranceschi\OneDrive - Lower Thames Crossing\02 May Reports"
+
+# Create a Streamlit file uploader
+uploaded_files = st.file_uploader("Upload files", accept_multiple_files=True)
+
+# Check if files are uploaded
+if uploaded_files:
+    # Iterate over each uploaded file
+    for file in uploaded_files:
+        # Get the file name
+        file_name = file.name
+        # Create the full path to save the file
+        file_path = os.path.join(target_directory, file_name)
+        # Save the file to the target directory
+        with open(file_path, "wb") as f:
+            f.write(file.read())
+        # Print a success message
+        st.success(f"{file_name} was successfully uploaded. THANK YOU!")
+
+
+#python -m streamlit run main.py
