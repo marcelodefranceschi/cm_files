9edc6efa1586e0223026b591008e6104943c1b24
marcelodefranceschi <151757219+marcelodefranceschi@users.noreply.github.com>
2024-08-26 12:06:26 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -5,31 +5,33 @@ import subprocess
 import hmac
 
 
-# Function to check credentials
-def check_credentials(username, password):
-    for user in st.secrets["credentials"].values():
-        if username == user["username"] and password == user["password"]:
-            return True
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
     return False
 
-# Streamlit app
-def main():
-    st.title("Login Page")
 
-    # Input fields for username and password
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
-    # Login button
-    if st.button("Login"):
-        if check_credentials(username, password):
-            st.success("Login successful!")
-            st.write("Welcome, ", username)
-        else:
-            st.error("Invalid username or password")
-
-if __name__ == "__main__":
-    main()
 
 
 
